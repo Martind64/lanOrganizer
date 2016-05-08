28b030a05568339956bf30f1c2e6ae59e48a4eaa
Martind64 <martind_64@hotmail.com>
2016-05-08 21:07:38 +0200

    added more

--- app/models/event.js
+++ app/models/event.js
@@ -5,9 +5,9 @@ var bcrypt		= require('bcrypt-nodejs')
 
 // EVENT SCHEMA
 var EventSchema = new Schema({
-	name:
-	address:
-	description:
+	name: String.
+	address: String, 
+	description: text,
 });
 
 
